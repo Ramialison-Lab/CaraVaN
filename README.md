161f67ab3d31f04e61bea59eb7ff14dd553e018d
dduygaucho <ngocduyt@student.unimelb.edu.au>
2025-08-26 09:12:18 +1000

    'struct'

--- README.md
+++ README.md
@@ -7,7 +7,21 @@ Gulrez Chahal, Ngoc Duy Tran, Svenja Pachernegg, Fernando Rossello, Sonika Tyagi
 ## Description
 CaraVaN (Cardiac Variants in Non-coding Genome) is a machine learning-based method to score potentially pathogenic single nucleotide variants in the non-coding genome pertaining to heart disease including congenital heart disease (CHD). We use ensemble approach (Catboost, Light gradient-boosting and Logistic regression) to score genome-wide probability scores for cardiac pathogenicity (where 0 is non-pathogenic and 1 is pathogenic). This model learns froma repertoire of cardiac specific genomic 198 features that have been curated from various genomic resources including cardiac-specific ChIP-sequencing datasets, HiC datasets, transcription factor binding sites.
 
-Languages: Python 3.8, Linux 
+Languages: Python 3.4, Linux 
+
+## Downloading the Cardiac-specific Non-coding genome Features
+The features used in the model can be downloaded from this [link](https://bridges.monash.edu/projects/CaraVaN_datasets/118794).
+
+## Getting the Source Code
+
+To get the source code, please click the "fork" button in the upper-right and then add this repo as an upstream source:
+
+````
+$ git clone -b master https://github.com/Ramialison-Lab/CaraVaN.git
+$ cd CaraVaN
+````
+
+
 
 
 # Bioinformatics Pipeline Template
---
+++ scripts/drop_dups_continuous_dickel.py
@@ -0,0 +1,49 @@
+# Library declaration
+import numpy as np
+import pandas as pd
+import sklearn
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--chromosome", type=str, required = True)
+args = parser.parse_args()
+
+# change the parameters here to update the run 
+CHROMOSOME = args.chromosome
+
+VALIDATION_FILE = f"continuous_merging/dickel_{CHROMOSOME}.txt"
+OUTPUT_FILE = f"continuous_merging/dickel_{CHROMOSOME}_drop_dups.txt"
+
+print("READ THE NCER FILE")
+df_ncer = pd.read_csv(VALIDATION_FILE, sep = '\t', header = None)
+
+
+# set the column names
+print("SETTING COLUMN NAMES")
+df_ncer.columns = ['chrom', 'POS', 'POS_INCR', 'chrom_ref', 'POS_REF', 'POS_INCR_REF', 'scores', 'success']
+
+
+# drop duplicates
+# df_ncer = df_ncer.drop_duplicates(subset=["chrom", "POS"])
+# df_ncer = df_ncer.sort_values(by = ['chrom', 'POS', 'POS_INCR'], 
+#                     ascending = [True, True, True])
+
+# convert scores to numeric, coerce errors to NaN
+df_ncer["scores"] = pd.to_numeric(df_ncer["scores"], errors="coerce")
+
+
+# keep the row with the maximum 'scores' for each (chrom, POS, POS_INCR)
+df_ncer = df_ncer.sort_values("scores", ascending=False)
+df_ncer = df_ncer.drop_duplicates(subset=["chrom", "POS", "POS_INCR"], keep="first")
+
+# sort the cleaned table again
+df_ncer = df_ncer.sort_values(by=['chrom', 'POS', 'POS_INCR'], ascending=[True, True, True])
+
+
+# convert NaN back to "."
+df_ncer["scores"] = df_ncer["scores"].fillna(".")
+
+print(df_ncer.shape)
+print(df_ncer.head())
+# df_ncer = df_ncer[['chrom', 'POS', 'POS_INCR', 'scores']]
+df_ncer.to_csv(OUTPUT_FILE, sep='\t', header=False, index=False)
